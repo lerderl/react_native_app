cd38dca8a8f6e1fa4013814015bc105ca66bff87
Joseph Olukunle <josepholukunle1107@gmail.com>
2023-01-27 07:59:10 +0100

    feat: Add few core components

--- App.js
+++ App.js
@@ -1,12 +1,35 @@
+import Remo from './assets/remo.png'
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    // <View style={styles.container}>
+    //   <TextInput>Open up App.js to start working on your app!</TextInput>
+    //   <StatusBar style="auto" />
+    // </View>
+    <ScrollView style={{ padding: 50 }}>
+      <Text>Clubs</Text>
+      <View>
+        <Text>Season</Text>
+        <Image
+          // source={{
+          //   uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
+          // }}
+          source={Remo}
+          style={{width: 200, height: 200, }}
+        />
+      </View>
+      <TextInput
+        style={{
+          height: 40,
+          borderColor: 'gray',
+          borderWidth: 1,
+        }}
+        defaultValue="Enter club name"
+      />
       <StatusBar style="auto" />
-    </View>
+    </ScrollView>
   );
 }
 
--- README.md
+++ README.md
@@ -6,3 +6,4 @@
   4. Scan the QR code using your mbile device and the the app will be up and running
   
   ![Image of App home screen](assets/home.jpeg)
+  
